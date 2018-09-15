fe1927b2d6004a0bbf60dc348c2d0983d1a4f35c
Zsolt Takacs <sajtosstangli@gmail.com>
2018-09-15 23:00:44 +0200

    separate existing test cases and add new ones for date taken

--- PicSortTest/PicSortTest.cs
+++ PicSortTest/PicSortTest.cs
@@ -12,28 +12,34 @@ namespace PicSortTest
         public void Test_ListAllPictures()
         {
             FileInfo [] fi = PicSortTools.ListAllPictures(@".\ListAllPictures", "*.jpg", true);
-            // There are 3 pictures in the folder
+            // There are 3 jpg pictures in the folder
             Assert.IsTrue(fi.Length == 3);
+
+            fi = PicSortTools.ListAllPictures(@".\ListAllPictures", "*.png", true);
+            Assert.IsTrue(fi.Length == 2);
         }
 
         [TestMethod]
-        public void Test_MovePicture()
+        public void Test_MovePicture_DesertJpg_To_2008_03()
         {
-            // Because the file copied in the test folder have the current date as modification date
-            string dateFolder = string.Format("PicSort_{0}_{1}", DateTime.Now.Year, DateTime.Now.Month.ToString("D2"));
-
             // The file already exists in the destination folder
             FileInfo fiDesert = new FileInfo(@".\MovePicture\Desert.jpg");
+            string dateFolder = string.Format("PicSort_{0}_{1}", 2008, "03");
             PicSortTools.MovePicture(fiDesert, @".\ResultsPictures", false);
 
             Assert.IsFalse(File.Exists(@".\MovePicture\Desert.jpg"));
             Assert.IsTrue(File.Exists(string.Format(@".\ResultsPictures\{0}\Desert.jpg", dateFolder)));
+        }
 
+        [TestMethod]
+        public void Test_MovePicture_KoalaAndPenguinsJpg_To_2008_02()
+        {
             // The file does not exists in the destination folder
             FileInfo fiKoala = new FileInfo(@".\MovePicture\Koala.jpg");
             PicSortTools.MovePicture(fiKoala, @".\ResultsPictures", false);
 
             Assert.IsFalse(File.Exists(@".\MovePicture\Koala.jpg"));
+            var dateFolder = string.Format("PicSort_{0}_{1}", 2008, "02");
             Assert.IsTrue(File.Exists(string.Format(@".\ResultsPictures\{0}\Koala.jpg", dateFolder)));
 
             FileInfo fiPenguins = new FileInfo(@".\MovePicture\Penguins.jpg");
@@ -41,6 +47,32 @@ namespace PicSortTest
 
             Assert.IsFalse(File.Exists(@".\MovePicture\Penguins.jpg"));
             Assert.IsTrue(File.Exists(string.Format(@".\ResultsPictures\{0}\Penguins.jpg", dateFolder)));
+        }
+
+        [TestMethod]
+        public void Test_MovePicture_TigerPng_To_2008_02()
+        {
+            var dateFolder = string.Format("PicSort_{0}_{1}", 2008, "02");
+            var fileName = "Tiger.png";
+
+            FileInfo fiTiger = new FileInfo($@".\MovePicture\{fileName}");
+            PicSortTools.MovePicture(fiTiger, @".\ResultsPictures", false);
+
+            Assert.IsFalse(File.Exists($@".\MovePicture\{fileName}"));
+            Assert.IsTrue(File.Exists(string.Format(@".\ResultsPictures\{0}\{1}", dateFolder, fileName)));
+        }
+
+        [TestMethod]
+        public void Test_MovePicture_PngWithoutDateTaken_To_ActualDate()
+        {
+            var dateFolder = string.Format("PicSort_{0}_{1}", DateTime.Now.Year, DateTime.Now.Month.ToString("D2"));
+
+            string fileName = "TigerWithoutDateTaken.png";
+            FileInfo fiTigerWithoutDateTaken = new FileInfo($@".\MovePicture\{fileName}");
+            PicSortTools.MovePicture(fiTigerWithoutDateTaken, @".\ResultsPictures", false);
+
+            Assert.IsFalse(File.Exists($@".\MovePicture\{fileName}"));
+            Assert.IsTrue(File.Exists(string.Format(@".\ResultsPictures\{0}\{1}", dateFolder, fileName)));
         }
     }
 }
